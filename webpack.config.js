7319f62ab5278390b6f1a5a2b963c3189965f9b2
Adam Batwash <adambat1899@gmail.com>
2018-07-04 18:54:05 +0200

    add babel loader for jsx

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,21 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015'],
+            presets: ['react']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack test app',
