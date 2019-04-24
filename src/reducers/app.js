e4739b72d28f62cd981d5548db5bc9375516ce9c
Adam Batwash <adambat1899@gmail.com>
2019-04-24 07:36:21 +0200

    Add Flow

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,10 +1,21 @@
-const initialState = {
+// @flow
+
+type State = {
+	message: string
+};
+
+type Action = {
+	type: string,
+	[string]: any
+};
+
+const initialState: State = {
   message: 'hello world'
 };
 
 export { initialState };
 
-const app = (state = initialState, { type, ...payload }) => {
+const app = (state: State = initialState, { type, ...payload }: Action) => {
   switch (type) {
     default:
       return state;
